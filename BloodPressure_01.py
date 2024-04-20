2c200d1be7500642b145929e30f05b550648742c
foltenyi <foltenyi@hotmail.com>
2024-04-19 20:38:40 -0700

    In the diff which file is which?

--- BloodPressure_01.py
+++ BloodPressure_01.py
@@ -48,10 +48,10 @@ If a line doesn't correspond to this, it is ignored.
 # def Usage(): ends
 
 
-def SetupBloodpressure(): # Into B
+def SetupBloodpressureAndGraph(): # Into B
 # The missing data represented by None or NaN ????
     global B, FileName, strt, end
-    
+
     with open(FileName) as f: # will close the file
         while (l := f.readline()): # E.g. a good line can be
         # 24/03/30 Sa 170.7 08:20 126/56/51 14:24 153/63/51 ==== ======
@@ -68,7 +68,7 @@ def SetupBloodpressure(): # Into B
             # else: # Executes if there was no break
             if not z.isdecimal():
                 continue # There was error, get next line
-                    
+
             d = list(map(int, x[0].split('/')))
             if strt <= d <= end:
                 if len(x) == 11:
@@ -76,6 +76,8 @@ def SetupBloodpressure(): # Into B
                 # Delete the 2nd, day, and 3rd, weight columns
                 x[1:3] = []
                 B.append(x)
+
+    B.reverse()
 
     # B is ready, fill out the missing data.
     # First step convert 'hh:mm' -> 60*hh + mm at index 1,3,5,7,9 if possible,
@@ -96,6 +98,10 @@ def SetupBloodpressure(): # Into B
             xnew += [j]
             if type(v := B[j][i]) == int:
                 x += [j]; y += [v]
+        if len(x) < 1:
+            sys.stderr.write(f'\n'+ln()+f' Column {i} does not have time!\n')
+            return
+
         ynew = np.interp(xnew, x, y)
 
         for j in range(len(B)):
@@ -128,8 +134,64 @@ def SetupBloodpressure(): # Into B
             l[i] = [int(snew[j]), int(dnew[j])]
             j += 1
 
-    pass # ???? Set break point here
-                         
+    # All data in B are filled. Just a reminder, there are two types of rows:
+    #      0        1       2      3      4        5      6        7      8       9     10
+    # ['24/03/31', 515, [139,57], 822, [137,60], 1128, [154,54], 1377, [184,80], '=' , '='],
+    # ['24/03/30', 500, [126,56], 864, [153,63], 1132, [172,66], 1388, [192,70], 1388,'Amlodipine']
+    #
+    # Prepare X for daily display and YS for systolic and YD for the diastolic measurements
+    X = []; YS = []; YD = []
+    x2r = 0  # How much to step to right on x from one to the other
+    minD = 100; maxS = 0  # For grid setting
+    for x in B:
+        X.append([x[1]-x[1]+x2r, x[3]-x[1]+x2r, x[5]-x[1]+x2r, x[7]-x[1]+x2r])
+        if type(x[9]) == int:  # There is a time and comment on this day
+            x[9] = x[9]-x[1]+x2r  # Modify the time, so it can be found in the transformed time
+        x2r = x[7]-x[1]+x2r
+        x2r += 16  # Leave a little gap between days
+        YS.append([x[2][0], x[4][0], x[6][0], x[8][0]])
+        if maxS < max(YS[-1]):
+            maxS = max(YS[-1])
+        YD.append([x[2][1], x[4][1], x[6][1], x[8][1]])
+        if minD > min(YD[-1]):
+            minD = min(YD[-1])
+
+    # Ready for display the graph
+    fig, ax = plt.subplots(figsize=(11.75, 8.25))  # In inches, W x H
+
+    ax.set_title('Vilmos Foltenyi blood pressure chart', fontsize=18)
+    _p1 = X[0][0]  # Generate 2 ticks, to the first and the last times
+    _p2 = X[-1][-1]+1
+    _p3 = X[-1][-1]
+    ax.set_xticks(np.arange(_p1, _p2, _p3), [B[0][0], B[-1][0]], fontsize=16)
+    ax.set_ylabel('Blood Pressure', fontsize=14)
+    for x, ys, yd in zip(X, YS, YD):
+        ax.plot(x, ys, color='#FF0000', label='Systolic'  if x[0]==X[0][0] else '')
+        ax.plot(x, yd, color='#0000FF', label='Diastolic' if x[0]==X[0][0] else '')
+
+    leg = ax.legend(fontsize=16, frameon=False)
+
+    for i in range(len(B)):
+        if type(x := B[i][9]) == int:
+            com = B[i][10]  # Comment goes into the graph
+            if X[i].count(x) < 1:
+                sys.stderr.write(f'\n'+ln()+f" Timestamp for {com} doesn't occur before!\n")
+                return
+
+            y = YS[i][X[i].index(x)]
+            ax.text(x, y, com, fontsize=14, clip_on=True, horizontalalignment='center')
+            ax.plot(x, y, 'o', color='#00F000', markersize=15)
+
+    ax.grid(axis='y', color='#800080', linestyle='--', linewidth=0.5)
+    step = 10
+    yticks = np.arange(step*(minD//step), step*(maxS//step+1), step)
+    ax.set_yticks(yticks)
+    # breakpoint()
+
+    plt.show()
+
+# END def SetupBloodpressureAndGraph(): # Into B
+
 
 # <bloodpressure file> <start or end date> <end or start date>
 def getParameters() -> bool:
@@ -146,7 +208,7 @@ def getParameters() -> bool:
         sys.stderr.write(ln() + f" {FileName=} doesn't exist\n")
         Usage()
         return False
-    
+
     try:
         file = open(FileName) # As line oriented text file
                               # Use file.readline()
@@ -201,14 +263,14 @@ def main():
         # <end or start date> date format: yy/mm/dd
         return # Complain was already printed
 
-    SetupBloodpressure()  # B is set up probably no missing data
+    SetupBloodpressureAndGraph()  # B is set up probably no missing data
 
     # FillMissingDataByInterpolation() # Using the global structure
 
 
 #######################################################################
 if __name__ == '__main__':
-    breakpoint()  # ???? DEBUG, to set other breakpoints
+    # breakpoint()  # ???? DEBUG, to set other breakpoints
     main()
     print('\n'+ln()+' Thanks for using this program, any suggestion is welcomed.\n\n')
 else:
